bfc1689cb55bec83e2f766d2fa54b8fd45685b59
leonliao8 <1324472473@qq.com>
2020-06-15 16:51:33 +0800

    提交所有

--- W3C/HTML/day01/showPic.js
+++ W3C/HTML/day01/showPic.js
@@ -8,3 +8,24 @@ function countBoduChildren(){
     alert (body_element.childNodes.length);
     window.onload = countBodyChildren;
 }
+
+
+
+function sort(elements) {
+    for(var i = 0; i < elements.length - 1; i++) {
+        for( var j = 0; j< elements.length -i - 1; j++){
+            if (elements[j] > elements[j+1]) {
+                var temp = elements[j];
+                elements[j] = elements[j+1];
+                elements[j+1] = temp;
+            }
+        }
+    }
+    return elements;
+}
+var elements = [1,24,34,5,6]
+document.writeln(elements);
+
+document.write(" \n ")
+sort(elements);
+document.write(elements);
